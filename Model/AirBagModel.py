e2c3eea636cb2a8bdc3fbbf82a758708bc594d07
Jarrod Bakker <jarrodbakker@hotmail.com>
2015-09-21 19:45:26 +1200

    File holding the model.

---
+++ Model/AirBagModel.py
@@ -0,0 +1,15 @@
+__author__ = 'jarrod'
+
+#
+# A model of an airport baggage handling system. This model is based
+# off of a model found noted in the paper Riga Airport Baggage Handling
+# System Simulation written by Mihails Savrasovs,Alexander Medvedev and
+# Elena Sincova. This was last accessed from
+# https://www.extendsim.com/downloads/papers/sols_papers_latviaBaggage.pdf
+# on September 21, 2015.
+#
+# SimPy is used to to simulate the system.
+#
+# At specific moments events will be triggered (NOT YET IMPLEMENTED)
+#
+
