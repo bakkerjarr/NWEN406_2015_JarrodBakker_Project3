80370cf6394b309566b4179fb6e980df30088b77
Jarrod Bakker <jarrodbakker@hotmail.com>
2015-09-22 21:37:39 +1200

    Supports command-line arguments.

--- Model/AirBagModel.py
+++ Model/AirBagModel.py
@@ -11,7 +11,14 @@
 # At specific moments events will be triggered (NOT YET IMPLEMENTED)
 #
 
+#
+# TODO - Allow the collection of queue statistics so that they may be
+#        processed by AWS Lambda, for example queue sizes and expected
+#        waiting times.
+#
+
 from CustomerGenerator import CustomerGenerator
+import argparse
 import simpy
 
 
@@ -24,16 +31,17 @@ class Model:
     :param max_cust - maximum number of customers that will be simulated.
     :param num_check_in - number of check-in counters.
     :param rand_seed - seed for the pseudorandom number generator.
+    :param time_facotr - scaling factor for real-time simluation.
     """
-    def __init__(self, arrival_rate, max_cust, num_check_in, rand_seed):
+    def __init__(self, arrival_rate, max_cust, num_check_in,
+                 rand_seed, time_factor):
         self._arrival_rate = arrival_rate
         self._max_cust = max_cust
         self._num_check_in = num_check_in
         self._rand_seed = rand_seed
 
         # Initialise the SimPy environment
-        self._env = simpy.Environment()
-        #self._env = simpy.rt.RealtimeEnvironment(factor=1.0)
+        self._env = simpy.rt.RealtimeEnvironment(factor=time_factor)
 
         # Create the necessary queues
         self._check_in_counters = []
@@ -57,10 +65,29 @@ class Model:
 
 
 if __name__ == "__main__":
-    # TODO - Get CLI arguments
-    arrival_rate = 5
-    max_cust = 20
-    num_check_in = 7
+    parser = argparse.ArgumentParser(description="Model an airport "
+                                                 "baggage handling "
+                                                 "system for "
+                                                 "departing customers.")
+    parser.add_argument("max_cust", metavar="N", type=int,
+                        help="The number of customers to be processed "
+                             "by the system.")
+    parser.add_argument("-c", dest="num_check_in", default=7,
+                        type=int, help="The number of check-in "
+                                       "counters (default is 7).")
+    parser.add_argument("-s", dest="time_factor", default=1, type=float,
+                        help="Scaling factor for real-time simulation "
+                             "(default is 1.0 or clock time.")
+
+    args = parser.parse_args()
+
+    arrival_rate = 4
     rand_seed = 999
-    m = Model(arrival_rate, max_cust, num_check_in, rand_seed)
+    m = Model(arrival_rate, args.max_cust, args.num_check_in,
+              rand_seed, args.time_factor)
+    print("[!] Starting simulation.\n\tTotal number of customers: {0}"
+          "\n\tNumber of check-in counters: {1}\n\tTime scaling "
+          "factor: {2}\n").format(args.max_cust, args.num_check_in,
+                                args.time_factor)
     m.start_simulation()
+    print("\n[!] Simulation complete.")
